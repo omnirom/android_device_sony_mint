0ef9ea43b5be0c45d687d146899d22cf428198f6
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-03-03 12:05:25 +0100

    mint: renamed kernel defconfig
    Change-Id: Ica066b768f279e41d9a87fc28a0cf6549d31f81f

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,7 +3,7 @@
 
 TARGET_SPECIFIC_HEADER_PATH += device/sony/mint/include
 
-TARGET_KERNEL_CONFIG := cyanogen_blue_mint_defconfig
+TARGET_KERNEL_CONFIG := cm_blue_mint_defconfig
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 18
