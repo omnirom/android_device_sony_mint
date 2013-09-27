eaac21140d578fc1161daa75cc593e498c3f9d0a
Lalit Maganti <lalitmaganti@gmail.com>
2013-09-27 14:10:25 +0200

    mint: initial commit for omni
    Change-Id: I019d99d8318ef3486342355bb0336b2938fc82bc

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,3 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/full_mint.mk
+    $(LOCAL_DIR)/full_mint.mk \
+    $(LOCAL_DIR)/custom_mint.mk
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_mint-eng
+add_lunch_combo custom_mint-eng
